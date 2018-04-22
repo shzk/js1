d1177c2730143ad4927d06449e0ba559ffcec7e3
Denis <den4@me.com>
2018-04-22 19:00:18 +0300

    personDone

--- js/script.js
+++ js/script.js
@@ -22,9 +22,11 @@ let popupBtn = document.getElementById('popup-btn'),
     candidateHair = 0,
     candidateShoes = 0,
     skinDiv = document.getElementsByClassName('skin')[0],
+    slidesSkin = skinDiv.getElementsByClassName('skin-color'),
     hairDiv = document.getElementsByClassName('hair')[0],
     slidesHair = hairDiv.getElementsByClassName('hair-style'),
-    clothesDiv = document.getElementsByClassName('clothes')[0];
+    clothesDiv = document.getElementsByClassName('clothes')[0],
+    slidesClothes = clothesDiv.getElementsByClassName('clothes-style');
 
 popupBtn.addEventListener('click', function(){
     overlay.style.display = 'none';
@@ -40,48 +42,64 @@ custom.addEventListener('change', function(){
     if (male.checked) {
         candidateSex = male.value;
         personSkin.classList.add('person-skin-1');
-        personSkin.classList.remove('person-skin-4');
-        personClothes.classList.add('person-clothes-1');
-        personClothes.classList.remove('person-clothes-4');
+        for (let i = 0; i < 3; i++) {
+            personSkin.classList.remove(`person-skin-${i+1}`);
+            slidesSkin[i].style.display = 'none';
+        }
+        slidesSkin[0].style.display = 'block';
         personHair.classList.add('person-hair-1');
         slidesHair[0].style.display = 'block';
         for (let i = 3; i < 6; i++) {
             personHair.classList.remove(`person-hair-${i+1}`);
             slidesHair[i].style.display = 'none';
         }
+        slidesClothes[0].style.display = 'block';
+        for (let i = 3; i < 6; i++) {
+            personClothes.classList.remove(`person-clothes-${i+1}`);
+            slidesClothes[i].style.display = 'none';
+        }
+        personClothes.classList.add('person-clothes-1');
+        personClothes.classList.remove('person-clothes-4');
     } else {
         candidateSex = female.value;
         personSkin.classList.add('person-skin-4');
-        personSkin.classList.remove('person-skin-1');
-        personClothes.classList.add('person-clothes-4');
-        personClothes.classList.remove('person-clothes-1');
+        for (let i = 0; i < 3; i++) {
+            personSkin.classList.remove(`person-skin-${i+1}`);
+            slidesSkin[i].style.display = 'none';
+        }
+        slidesSkin[0].style.display = 'block';
         personHair.classList.add('person-hair-4');
         slidesHair[3].style.display = 'block';
         for (let i = 0; i < 3; i++) {
             personHair.classList.remove(`person-hair-${i+1}`);
             slidesHair[i].style.display = 'none';
         }
+        slidesClothes[3].style.display = 'block';
+        for (let i = 0; i < 3; i++) {
+            personClothes.classList.remove(`person-clothes-${i+1}`);
+            slidesClothes[i].style.display = 'none';
+        }
+        personClothes.classList.add('person-clothes-4');
+        personClothes.classList.remove('person-clothes-1');
     }
 });
 
 function skinSlider() {
-    let slideIndex = 1,
-        slides = skinDiv.getElementsByClassName('skin-color'),
+    let slideSkinIndex = 1,
         prev = skinDiv.querySelector('.prev'),
         next = skinDiv.querySelector('.next');
 
-    showSlides(slideIndex);
+    showSlides(slideSkinIndex);
 
     function showSlides(n){
-        if (n > slides.length) {
-            slideIndex = 1;
-            console.log(`n > slideIndex = ${slideIndex}`);
+        if (n > slidesSkin.length) {
+            slideSkinIndex = 1;
         };
         if (n < 1) {
-            slideIndex = slides.length;
+            slideSkinIndex = slidesSkin.length;
         };
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
+        for (let i = 0; i < slidesSkin.length; i++) {
+            slidesSkin[i].style.display = 'none';
             if (male.checked) {
                 for (let j = 1; j < 4; j++) {
                     personSkin.classList.remove(`person-skin-${j}`);
@@ -92,19 +110,19 @@ function skinSlider() {
                 }
             }
         };
-        slides[slideIndex - 1].style.display = 'block';
+        slidesSkin[slideSkinIndex - 1].style.display = 'block';
         if (male.checked) {
-            personSkin.classList.add(`person-skin-${slideIndex}`);
-            candidateSkin = `person-skin-${slideIndex}`;
+            personSkin.classList.add(`person-skin-${slideSkinIndex}`);
+            candidateSkin = `person-skin-${slideSkinIndex}`;
             // console.log(candidateSkin);
         } else {
-            personSkin.classList.add(`person-skin-${slideIndex+3}`);
-            candidateSkin = `person-skin-${slideIndex+3}`;
+            personSkin.classList.add(`person-skin-${slideSkinIndex+3}`);
+            candidateSkin = `person-skin-${slideSkinIndex+3}`;
             // console.log(candidateSkin);
         }
     };
     function plusSlides(n) {
-        showSlides(slideIndex += n);
+        showSlides(slideSkinIndex += n);
     };
     // function currentSlide(n) {
     //     showSlides(slideIndex = n);
@@ -184,3 +202,68 @@ function hairSlider() {
         });
     };
 hairSlider();
+
+function clothesSlider() {
+    let slideClothesIndex = 1,
+        prevClothes = clothesDiv.querySelector('.prev'),
+        nextClothes = clothesDiv.querySelector('.next'); 
+
+    showClothesSlides(slideClothesIndex);
+
+    function showClothesSlides(n){
+
+        if (male.checked) {
+            for (let k = 3; k < slidesClothes.length; k++) {
+                slidesClothes[k].style.display = 'none';
+                personClothes.classList.remove(`person-clothes-${k+1}`);
+            }
+            if (n > slidesClothes.length-3) {
+                slideClothesIndex = 1;
+            };
+            if (n < 1) {
+                slideClothesIndex = slidesClothes.length-3;
+            };
+            for (let i = 0; i < slidesClothes.length-3; i++) {
+                slidesClothes[i].style.display = 'none';
+                for (let j = 1; j < 4; j++) {
+                    personClothes.classList.remove(`person-clothes-${j}`);
+                }
+            };
+            slidesClothes[slideClothesIndex - 1].style.display = 'block';
+            personClothes.classList.add(`person-clothes-${slideClothesIndex}`);
+            candidateClothes = `person-clothes-${slideClothesIndex}`;
+        } else {
+            for (let k = 0; k < slidesClothes.length-3; k++) {
+                slidesClothes[k].style.display = 'none';
+                personClothes.classList.remove(`person-clothes-${k+1}`);
+            }
+            if (n > slidesClothes.length) {
+                slideClothesIndex = 4;
+            };
+            if (n < 4) {
+                slideClothesIndex = slidesClothes.length;
+            };
+            for (let i = 3; i < slidesClothes.length; i++) {
+                slidesClothes[i].style.display = 'none';
+                for (let j = 4; j < 7; j++) {
+                    personClothes.classList.remove(`person-clothes-${j}`);
+                }
+            };
+            slidesClothes[slideClothesIndex - 1].style.display = 'block';
+            personClothes.classList.add(`person-clothes-${slideClothesIndex}`);
+            candidateClothes = `person-clothes-${slideClothesIndex}`;
+        }
+    };
+
+        function plusClothesSlides(n) {
+            showClothesSlides(slideClothesIndex += n);
+        };
+
+        prevClothes.addEventListener('click', function(){
+            plusClothesSlides(-1);
+        });
+        nextClothes.addEventListener('click', function(){
+            plusClothesSlides(1);
+        });
+    };
+clothesSlider();
